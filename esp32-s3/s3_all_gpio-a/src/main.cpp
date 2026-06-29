0bd68238bcfc5ba7ae4ebc72c5695878191d8dcc
wa1tnr <wa1tnr@users.noreply.github.com>
2026-06-29 21:30:15 +0000

    blind commit untested

--- esp32-s3/s3_all_gpio-a/src/main.cpp
+++ esp32-s3/s3_all_gpio-a/src/main.cpp
@@ -1,5 +1,5 @@
-/* Sun 28 Jun 18:17:48 UTC 2026 vscode */
-/* blink D2 thru D7 nora - arduino nano esp32 */
+/* Mon 29 Jun 21:24:05 UTC 2026 vscode */
+/* blink them all esp32s3 native wokwi */
 
 #include <Arduino.h>
 
@@ -17,16 +17,11 @@ void blink(uint8_t pin) {
   half_blink(pin);
 }
 
-
-      // "attrs": { "text": "L T to B:   4 5 6 7 15 16 17 18 8 3 46 9-14" }
-
-      // "attrs": { "text": "19 20 21 47 48 45 0 35-42 2 1   R: B to T" }
-
-
 void setup_gpio() {
-  int pinsX[] = {2, 3, 4, 5, 6, 7};
-  int pinsQ[] = {5, 6, 7, 8, 9, 10};
-  int pins[] = {4,5,6,7,15,16,17,18,8,3,46,9,10,11,12, 13,14,19,20,21,47,48,45,0,35,36,37,38,39,40,42,42,2,1};
+  int pins[] = {4,  5,  6,  7,  15, 16, 17, 18, 8,
+                3,  46, 9,  10, 11, 12, 13, 14, 19,
+                20, 21, 47, 48, 45, 0,  35, 36, 37,
+                38, 39, 40, 41, 42, 2,  1};
   for (int pin : pins) {
     pinMode(pin, OUTPUT);
     digitalWrite(pin, HIGH);
@@ -34,8 +29,10 @@ void setup_gpio() {
 }
 
 void blink_all() {
-  int pinsQ[] = {5, 6, 7, 8, 9, 10};
-  int pins[] = {4,5,6,7,15,16,17,18,8,3,46,9,10,11,12, 13,14,19,20,21,47,48,45,0,35,36,37,38,39,40,42,42,2,1};
+  int pins[] = {4,  5,  6,  7,  15, 16, 17, 18, 8,
+                3,  46, 9,  10, 11, 12, 13, 14, 19,
+                20, 21, 47, 48, 45, 0,  35, 36, 37,
+                38, 39, 40, 41, 42, 2,  1};
   for (int pin : pins) {
     blink(pin);
     delay(500);
